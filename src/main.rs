5f9e5b5d18c788314df7748aa99b1a47bc0b5d37
Zachary Petti <zachmpetti@gmail.com>
2023-07-07 11:36:30 -0400

    Stricter clippy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![warn(clippy::unwrap_used)]
-#![warn(clippy::perf, clippy::disallowed_types)]
+#![warn(clippy::perf, clippy::disallowed_types)] // performance warns
+#![warn(clippy::pedantic)]
 // most bevy systems violate these. Nothing I can do about it at the moment.
-#![allow(clippy::type_complexity, clippy::too_many_arguments)]
+#![allow(
+    clippy::type_complexity,
+    clippy::too_many_arguments,
+    // clippy::needless_pass_by_value // TODO: separate out system functions from non-system 
+)]
 
 mod console;
 mod controllers;
